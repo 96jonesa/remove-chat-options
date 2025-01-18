881f8ef8a478503cd02f6554a6bcbf9d194ad85d
Andrew Jones <ajones96@uw.edu>
2025-01-18 15:23:28 -0800

    use InterfaceID.CHATBOX instead of explicit options list

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Removes all Add friend, Add ignore, Message, and Report menu options. Currently,
-this also removes the Message menu option from the user's friends list.
+Removes all chatbox menu options.
 
 Useful when the chatbox is transparent, allowing the user to interact with the rest of
 the game as if the chatbox were not there at all.
--- src/main/java/com/removechatoptions/RemoveChatOptionsPlugin.java
+++ src/main/java/com/removechatoptions/RemoveChatOptionsPlugin.java
@@ -5,46 +5,39 @@ import javax.inject.Inject;
 import net.runelite.api.Client;
 import net.runelite.api.MenuEntry;
 import net.runelite.api.events.MenuEntryAdded;
+import net.runelite.api.widgets.InterfaceID;
+import net.runelite.api.widgets.WidgetUtil;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 @PluginDescriptor(
 	name = "Remove Chat Options",
-	description = "Remove Add friend, Add ignore, Message, and Report menu options",
+	description = "Remove all chatbox menu options",
 	tags = {"chat"}
 )
 public class RemoveChatOptionsPlugin extends Plugin {
-	private static final String ADD_FRIEND = "Add friend";
-	private static final String ADD_IGNORE = "Add ignore";
-	private static final String MESSAGE = "Message";
-	private static final String REPORT = "Report";
-
-	private static final Set<String> OPTIONS_TO_REMOVE = Set.of(ADD_FRIEND, ADD_IGNORE, MESSAGE, REPORT);
-
 	@Inject
 	private Client client;
 
 	@Subscribe
-	public void onMenuEntryAdded(MenuEntryAdded entry) {
-		String option = entry.getOption();
+	public void onMenuEntryAdded(MenuEntryAdded menuEntryAdded) {
+		MenuEntry[] menuEntries = client.getMenuEntries();
 
-		if (OPTIONS_TO_REMOVE.contains(option)) {
-			MenuEntry[] menuEntries = client.getMenuEntries();
+		List<MenuEntry> newMenuEntriesList = new LinkedList<>();
 
-			List<MenuEntry> newMenuEntriesList = new LinkedList<>();
+		for (MenuEntry menuEntry : menuEntries) {
+			final int componentId = menuEntry.getParam1();
+			int groupId = WidgetUtil.componentToInterface(componentId);
 
-			for (MenuEntry menuEntry : menuEntries) {
-				if (!menuEntry.getOption().equals(option)) {
-					newMenuEntriesList.add(menuEntry);
-				}
+			if (groupId != InterfaceID.CHATBOX) {
+				newMenuEntriesList.add(menuEntry);
 			}
-
-			client.setMenuEntries(newMenuEntriesList.toArray(new MenuEntry[0]));
 		}
+
+		client.setMenuEntries(newMenuEntriesList.toArray(new MenuEntry[0]));
 	}
 }
